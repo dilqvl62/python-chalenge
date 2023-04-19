d283dfb246e9d7aec0619378a739aabd4f1ea4b7
dilqvl62 <mourisabar@gmail.com>
2023-04-18 22:56:20 -0500

    calculating the net total for the entire period

--- PayBank/main.py
+++ PayBank/main.py
@@ -3,16 +3,20 @@ import os
 import csv
 csvpath = os.path.join('Resources', 'budget_data.csv')
 Total_Months = 0
+net_total = 0
+Profit_loses_list = []
 print("Financial Analysis")
 print("------------------------------")
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
-
-
-
+    P_l= csv_header.index("Profit/Losses")
+    Date = csv_header.index("Date")
     for row in csvreader:
         Total_Months +=1
+        net_total += int(row[P_l])
+        Profit_loses_list.append(row[P_l])
     print(f"Total Months: {Total_Months}")
-        
+    print (f"Total: ${net_total}") 
+    print (f"Greatest Increase in Profits: {net_total}")  
    
