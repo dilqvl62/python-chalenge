17bd298381168c89f4372be39d09e4872f50dc44
dilqvl62 <mourisabar@gmail.com>
2023-04-18 21:27:26 -0500

    calculated the total number of months inthe PayBank dataset

--- PayBank/main.py
+++ PayBank/main.py
@@ -1,6 +1,18 @@
 
 import os 
 import csv
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('Resources', 'budget_data.csv')
+Total_Months = 0
+print("Financial Analysis")
+print("------------------------------")
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+
+
+
+    for row in csvreader:
+        Total_Months +=1
+    print(f"Total Months: {Total_Months}")
+        
+   
