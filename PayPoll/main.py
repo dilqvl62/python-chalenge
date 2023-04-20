3c5f23ef4637c4b93dee7d05f29ac907d3c9e4b6
dilqvl62 <mourisabar@gmail.com>
2023-04-20 08:33:25 -0500

    printing the winner

--- PayPoll/main.py
+++ PayPoll/main.py
@@ -3,10 +3,12 @@ import csv
 
 csvpath = os.path.join('Resources', 'election_data.csv')
 output_path =  os.path.join('analysis', 'analysis.txt')
-#Declaring empty lists 
+#Declaring empty list to store all the votes by name 
 votes_list =[]
 #initialize a list 
 Candidate_list=[]
+#total votes list 
+total_votes=[]
 #opening an output file 
 with open(output_path,'w') as f:
     f.write("Election Results\n")
@@ -17,9 +19,10 @@ with open(output_path,'w') as f:
         print("---------------------")
         csvreader = csv.reader(csvfile,delimiter=',')
         csv_header = next(csvreader)
-        #Declaring index of the header list 
         
+        #Declaring index of the header list       
         Cand = csv_header.index("Candidate")
+
         #creating a list of all votes by candidate 
         for row in csvreader:
             votes_list.append(row[Cand])
@@ -28,12 +31,16 @@ with open(output_path,'w') as f:
         print("----------------------------------")
         
         #create a list of every candidate in a given list      
-        # by itterate through all elements in a given list
+        # by itterate through all elements in that list
         for v in votes_list:
             if v not in Candidate_list:
                 Candidate_list.append(v)
         
-        # Calculating the percentage of votes each candidate won 
+        # Calculating the percentage of votes each candidate won with the names and the total votes printed 
         for x in Candidate_list:
+            total_votes.append(votes_list.count(x))
             print(f"{x} : {'{0:}%'.format(round((votes_list.count(x)/len(votes_list))*100,3))} ({votes_list.count(x)})")
 
+        print("--------------------")
+      
+        print(f"Winner: {Candidate_list[total_votes.index(max(total_votes))]}")
