ea62b3e5b5f42daaf1f99b62463c3f7da761ad8f
dilqvl62 <mourisabar@gmail.com>
2023-04-20 07:13:46 -0500

    calculated the total votes and creating a candidate list

--- PayPoll/main.py
+++ PayPoll/main.py
@@ -4,7 +4,9 @@ import csv
 csvpath = os.path.join('Resources', 'election_data.csv')
 output_path =  os.path.join('analysis', 'analysis.txt')
 #Declaring empty lists 
-Ballot_Id =[]
+votes_list =[]
+#initialize a list 
+Candidate_list=[]
 #opening an output file 
 with open(output_path,'w') as f:
     f.write("Election Results\n")
@@ -13,4 +15,22 @@ with open(output_path,'w') as f:
     with open(csvpath) as csvfile:
         print("Election Results")
         print("---------------------")
+        csvreader = csv.reader(csvfile,delimiter=',')
+        csv_header = next(csvreader)
+        #Declaring index of the header list 
+        
+        Cand = csv_header.index("Candidate")
+        #creating a list of all votes by candidate 
+        for row in csvreader:
+            votes_list.append(row[Cand])
+
+        print(f" Total Votes: {len(votes_list)}")
+        print("----------------------------------")
+        
+        #create a list of unique values in a given list      
+        # by itterate through all elements in a given list
+        for x in votes_list:
+            if x not in Candidate_list:
+                Candidate_list.append(x)
+    print(Candidate_list)
 
